4068f15db79cb2b92a29bb9a8d2815fcf1e241fe
egarelnabi <eibashir@yahoo.com>
2024-05-08 11:47:51 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -129,7 +129,7 @@ resource "aws_instance" "hashicat" {
   tags = {
     Name = "${var.prefix}-hashicat-instance"
     Department = "${var.department}"
-    Billable = "${var.billable}"
+    Billable = "false"
   }
 }
 
